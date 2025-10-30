9c2e01e6ad34d62d172004a913c36224c86627d2
dhwang-duck <149436170+dhwang-duck@users.noreply.github.com>
2025-10-30 12:56:25 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [
@@ -19,7 +14,6 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "assets"),
     },
   },
-  root: path.resolve(__dirname),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
